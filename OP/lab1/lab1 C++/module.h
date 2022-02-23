69eae7db5abe1dcdb06ad4f8f1b1e6c05651ec6d
Vartan14 <89924019+Vartan14@users.noreply.github.com>
2022-02-23 23:26:17 +0200

    Add files via upload

---
+++ OP/lab1/lab1 C++/module.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <fstream>
+using namespace std;
+
+void createFile(string);
+string nameTheFile();
+void outputFile(string);
+void editFile(string,string);
